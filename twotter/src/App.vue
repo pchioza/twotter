e6d6032daa8ceb8934c9954f44435859aa8afdf7
Pempho Chioza <pchioza@gmail.com>
2021-01-25 22:26:31 +0200

    initial commit
    foundation

---
+++ twotter/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    @{{user.username}} - {{fullName}}
+    <strong>Followers: {{followers}}</strong>
+    <button @click="followUser">Follow</button>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      followers:0,
+      user:{
+        id:1,
+        username:'pchioza',
+        firstName:'Pempho',
+        lastName:'Chioza',
+        email:'pchioza@gmail.com', 
+        isAdmin:true
+      }
+    }
+  }, 
+  watch:{
+    followers(newFollowerCount, oldFollowerCount){
+      if(oldFollowerCount<newFollowerCount){
+        console.log(`${this.user.username} has gained a follower`)
+      }
+    }
+  },
+  computed:{
+    fullName(){
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods:{
+    followUser(){
+      this.followers++
+    }
+  },
+  mounted(){
+    this.followUser();
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+}
+</style>
